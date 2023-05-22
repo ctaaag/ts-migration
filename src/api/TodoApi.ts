2d60340cce77e450f26a44e276712c0b1badb91e
ctaaag <qudxor102@gmail.com>
2023-05-22 22:16:01 +0900

    📚DOCS: readme 추가

--- src/api/TodoApi.ts
+++ src/api/TodoApi.ts
@@ -1,6 +1,7 @@
 const API_URL = 'https://todo-api.roto.codes';
 export default class TodoApi {
-
+  requestURL:string;
+  
   constructor(userName:any) {
     this.requestURL = `${API_URL}/${userName}`;
   }
